e6100d00f99e589da8f8b8d85217e25746418b47
sophosnotificationtest-max <sophosnotificationtest@gmail.com>
2026-03-14 10:18:25 -0300

    Add i18.js for internationalization configuration

---
+++ i18.js
@@ -0,0 +1,8 @@
+module.exports = {
+  locales: ["en", "da", "de", "es", "fr", "no", "pt", "se", "tl", "is", "it", "he", "tr", "hu", "th", "zh-CN"],
+  defaultLocale: "en",
+  pages: {
+    "*": ["peppermint"],
+  },
+  localeDetection: true,
+};
